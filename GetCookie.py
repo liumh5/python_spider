f1ae7ffd2f3ac360122fcc1ac33e520157349eb9
liumh5 <534343957@qq.com>
2019-08-16 10:42:04 +0800

    20190816 commit;

--- GetCookie.py
+++ GetCookie.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 import os
 import urllib
 
@@ -7,17 +8,25 @@ from selenium import webdriver
 from time import sleep
 
 # 获取登录后的cookies
-
+from selenium.webdriver.firefox.options import Options
 
 
 class GetCookie:
     # 初始化浏览器和网址
-    def __init__(self, url,stime):
+    def __init__(self, url,stime,viewflag):
         self.url = url
-        self.driver = webdriver.Firefox()
+        if viewflag == 1 :
+            self.firefox_opt = Options()
+            self.firefox_opt.add_argument("--headless")
+            self.firefox_opt.add_argument("--disable-gpu")
+            self.firefox_opt.add_argument("--window-size=1366,768")
+            self.driver = webdriver.Firefox(firefox_options=self.firefox_opt)
+        else:
+            self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(stime)
         self.r = self.driver.get(self.url)
 
-        sleep(stime)
+        # sleep(stime)
     # 通过用户密码登陆网址
     def login(self,name,passwd,stime):
         self.driver.find_element_by_xpath("//input[@name='userName']").send_keys(name)
@@ -26,11 +35,22 @@ class GetCookie:
         sleep(stime)
     #  获取原始cookie数据
     def phone_login(self,phoneid,stime):
+        print(self.driver.page_source)
         self.driver.find_element_by_class_name("input-container").send_keys(phoneid)
         self.driver.find_element_by_class_name("m-btn-blue-hover").click()
-        sleep(stime)
+        print(self.driver.page_source)
+        if self.driver.find_element_by_class_name("number-code"):
+            print("输入收到的短信验证码")
+            ycode = input()
+            self.driver.find_element_by_class_name("number-code").send_keys(ycode)
+            if self.driver.find_element_by_class_name("m-btn-blue") :
+                self.driver.find_element_by_class_name("m-btn-blue").click()
+        # sleep(stime)
 
 
+    def get_headers(self):
+        logs = [json.loads(log['message'])['message'] for log in self.driver.get_log('performance')]
+        return logs;
     def get_cookie(self):
         cookie = self.driver.get_cookies()
         print(cookie)
@@ -70,7 +90,7 @@ class GetCookie:
            url = self.driver.find_element_by_class_name("login_content").find_element_by_tag_name("img").get_attribute("src")
            self.get_picture(url,pic_name)
            self.show_picture(pic_name)
-        sleep(10)
+        # sleep(10)
 if __name__ == '__main__':
     # url = 'https://weibo.com/'
     # c = GetCookie(url,20)
@@ -79,6 +99,12 @@ if __name__ == '__main__':
     # # c.get_cookie()
     # c.get_dict_cookie()
     url = "https://m.weibo.cn/login?backURL=https%253A%252F%252Fm.weibo.cn%252F"
-    c = GetCookie(url,20)
-    c.phone_login("17610359821",20)
-    c.get_dict_cookie()
+    c = GetCookie(url, 20, 1)
+    c.get_cookie()
+    try:
+        c.phone_login("18612416684",60)
+        c.get_dict_cookie()
+        c.driver.quit()
+        # print(c.get_headers())
+    except:
+        c.driver.quit()
